675e244a9c1de2bee07670264d57c65af10e6d4c
Max Ehnert <max.ehnert@gmail.com>
2016-04-05 19:15:00 -0500

    updated server.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const Hapi = require('hapi');
 const Good = require('good');
+const Joi  = require('joi');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: 3001 });
 
 server.route({
     method: 'GET',
@@ -14,11 +15,22 @@ server.route({
     }
 });
 
+// server.route({
+//     method: 'GET',
+//     path: '/{name}',
+//     handler: function (request, reply) {
+//         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+//     }
+// });
+
 server.route({
     method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    path: '/{yourname*}',
+    config: {  // validate will ensure YOURNAME is valid before replying to your request
+        validate: { params: { yourname: Joi.string().max(40).min(2).alphanum() } },
+        handler: function (req,reply) {
+            reply('Hello '+ req.params.yourname + '!');
+        }
     }
 });
 

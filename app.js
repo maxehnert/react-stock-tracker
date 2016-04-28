1de4b5126e6fe1dcf34f967c94ebcff5e5e08f48
Max Ehnert <max.ehnert@gmail.com>
2016-04-28 18:53:27 -0500

    Added filters to the scraped return value.

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
 var Xray = require("x-ray");
-var xray = new Xray();
-
-// xray('http://finance.yahoo.com/q/op?s=FB+Options', '.quote-table-overflow tr td a',
-//   [{
-//     option: '', //innerHTML
-//     href: '@href',
-//     prices : xray(
-//       '.quote-table-overflow tr td .option-entry'
-//     )
-//   }]
-// )
-// .write('results.json');
+var xray = new Xray({
+  filters: {
+    trim: function(value) {
+      return typeof value === 'string' ? value.trim() : value;
+    }
+  }
+});
 
 xray('http://finance.yahoo.com/q/op?s=FB+Options', {
   stockLast: xray('.time_rtq_ticker span'),
   expiration: xray('.Start-0 option'),
   calls: xray('#optionsCallsTable .quote-table-overflow tr', 
     [{
-      strikeValue: xray('td:nth-child(1) a'),
-      last: xray('td:nth-child(3)'),
-      bid: xray('td:nth-child(4)'),
-      ask: xray('td:nth-child(5)'),
-      change: xray('td:nth-child(6)'),
-      percentChange: xray('td:nth-child(7)'),
-      volume: xray('td:nth-child(8)'),
-      openInterest: xray('td:nth-child(9)'),
-      impliedVolatility: xray('td:nth-child(10)')
+      strikeValue: xray('td:nth-child(1) a  | trim'),
+      last: xray('td:nth-child(3) | trim'),
+      bid: xray('td:nth-child(4) | trim'),
+      ask: xray('td:nth-child(5) | trim'),
+      change: xray('td:nth-child(6) | trim'),
+      percentChange: xray('td:nth-child(7) | trim'),
+      volume: xray('td:nth-child(8) | trim'),
+      openInterest: xray('td:nth-child(9) | trim'),
+      impliedVolatility: xray('td:nth-child(10) | trim')
     }]
   ),
   puts: xray('#optionsPutsTable .quote-table-overflow tr', 
     [{
-      strikeValue: xray('td:nth-child(1) a'),
-      last: xray('td:nth-child(3)'),
-      bid: xray('td:nth-child(4)'),
-      ask: xray('td:nth-child(5)'),
-      change: xray('td:nth-child(6)'),
-      percentChange: xray('td:nth-child(7)'),
-      volume: xray('td:nth-child(8)'),
-      openInterest: xray('td:nth-child(9)'),
-      impliedVolatility: xray('td:nth-child(10)')
+      strikeValue: xray('td:nth-child(1) a | trim'),
+      last: xray('td:nth-child(3) | trim'),
+      bid: xray('td:nth-child(4) | trim'),
+      ask: xray('td:nth-child(5) | trim'),
+      change: xray('td:nth-child(6) | trim'),
+      percentChange: xray('td:nth-child(7) | trim'),
+      volume: xray('td:nth-child(8) | trim'),
+      openInterest: xray('td:nth-child(9) | trim'),
+      impliedVolatility: xray('td:nth-child(10) | trim')
     }]
   ),
 })
